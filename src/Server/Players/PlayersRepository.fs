0f6e3f3763ed91a524e84b82b4cca385d4522a2f
David Sanromá <dsanroma@gmail.com>
2020-04-21 16:05:57 +0200

    getOrCreateByName with helper inner functions.

--- src/Server/Players/PlayersRepository.fs
+++ src/Server/Players/PlayersRepository.fs
@@ -44,20 +44,38 @@ module Database =
     }
 
   let getOrCreateByName connectionString (PlayerName name) : Task<Result<Player, exn>> =
-    task {
-      let! existingPlayer = getByName connectionString (PlayerName name)
-      match existingPlayer with
-      | Error ex -> return Error ex
-      | Ok (Some player) -> return Ok player
-      | Ok None ->
+    // Helper functions
+    let condExec f m = task {
+        match m with
+        | Error ex -> return Error ex
+        | Ok (Some player) -> return Ok (Some player)
+        | _ -> return! f()
+    }
+
+    let checkPlayerExists() = task {
+        let! existingPlayer = getByName connectionString (PlayerName name)
+        match existingPlayer with
+        | Error ex -> return Error ex
+        | Ok (Some player) -> return Ok (Some player)
+        | Ok None -> return Ok None
+    }
+
+    let tryToInsertPlayer() = task {
         let! affectedRows = insert connectionString name
         match affectedRows with
         | Error ex -> return Error ex
-        | Ok _ ->
-            let! player = getByName connectionString (PlayerName name)
-            match player with
-            | Ok (Some p) -> return Ok p
-            | Ok None -> return Error (exn "Player added but not found on a second look.")
-            | Error ex -> return Error ex
+        | Ok _ -> return Ok None
     }
+    // ----------------
 
+    // Logic main body
+    task {
+        let! myResult1 = checkPlayerExists()
+        let! myResult2 = condExec tryToInsertPlayer myResult1
+        let! myResult3 = condExec checkPlayerExists myResult2
+
+        match myResult3 with
+        | Error ex -> return Error ex
+        | Ok (Some player) -> return Ok player
+        | _ -> return Error (exn "Player added but not found on a second look.")
+    }
